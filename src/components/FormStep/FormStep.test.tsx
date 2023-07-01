4f8492c2275a06b275fc97750dbbba2fda23ba25
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-07-01 11:44:49 -0500

    upgrade typescript back to version 5, ensure cypress works, ignore cypress artifacts, set user vent to return default for now

---
+++ src/components/FormStep/FormStep.test.tsx
@@ -0,0 +1,7 @@
+import { test } from 'vitest'
+import { render } from '../../../test/utilities.ts'
+import FormStep from './FormStep.tsx'
+
+test('component should render', () => {
+  render(<FormStep />)
+})
---
+++ src/components/FormStep/FormStep.tsx
@@ -0,0 +1,9 @@
+const FormStep = () => {
+  return (
+    <div>
+      <p>form step</p>
+    </div>
+  )
+}
+
+export default FormStep
