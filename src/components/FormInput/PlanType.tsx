121e35564047b81716e39086da6999f64d7a3e34
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-07-03 08:10:30 -0500

    create various components of differnet input types to be used in each fieldset

---
+++ src/components/FormInput/PlanType.tsx
@@ -0,0 +1,17 @@
+const PlanType = () => {
+  return (
+    <div>
+      <label>
+        type
+        <input type="radio" name="planType" value="timeOne" />
+      </label>
+      <div></div>
+      <label>
+        type
+        <input type="radio" name="planType" value="timeTwo" />
+      </label>
+    </div>
+  )
+}
+
+export default PlanType
---
+++ src/components/FormInput/index.tsx
@@ -0,0 +1,4 @@
+export { default as TextInput } from './TextInput.tsx'
+export { default as AddOnInput } from './AddOnInput.tsx'
+export { default as PlanInput } from './PlanInput.tsx'
+export { default as PlanType } from './PlanType.tsx'
