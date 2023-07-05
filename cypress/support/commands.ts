4721a9d6b18e924a0a136493cbc48f7d40d142ae
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-07-05 08:51:12 -0500

    create commands to complete certain steps of the form

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -35,3 +35,29 @@
 //     }
 //   }
 // }
+
+// create steps for completing a step of the form
+Cypress.Commands.add('completePersonalInfoStep', () => {
+  cy.get('[data-test="nameInput"]').type('Test Name')
+  cy.get('[data-test="emailInput"]').type('testmeail@gmail.com')
+  cy.get('[data-test="phoneInput"]').type('1234567890')
+  cy.get('[data-test="nextButton"]').click()
+})
+
+Cypress.Commands.add('completePlanStep', () => {
+  cy.get('[data-test=""arcadePlanInput]').click()
+  cy.get('[data-test="montlySubInput"]').click()
+  cy.get('[data-test="nextButton"]').click()
+})
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      completePersonalInfoStep(): Chainable<void>
+      completePlanStep(): Chainable<void>
+    }
+  }
+}
+
+export {}
