5e64d9123b6e92daa68d2c97fdfa9cd79098b1b3
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-07-14 08:16:09 -0500

    update AddOnINput to handle form values, form can be updated and submission logs the data

--- src/components/Form/AddOnSection/AddOnSection.tsx
+++ src/components/Form/AddOnSection/AddOnSection.tsx
@@ -1,21 +1,39 @@
+import { SubmitHandler, useForm } from 'react-hook-form'
+import type { MultiStepForm } from '@/src/types/form.ts'
 import AddOnInput from '../../FormInput/AddOnInput.tsx'
 
+export type AddOnForm = Pick<
+  MultiStepForm,
+  'onlineService' | 'largerStorage' | 'customProfile'
+>
+
 const AddOnSection = () => {
+  const { control, handleSubmit } = useForm<AddOnForm>({
+    defaultValues: {
+      onlineService: false,
+      largerStorage: false,
+      customProfile: false,
+    },
+  })
+
+  const onSubmit: SubmitHandler<AddOnForm> = (data) => {
+    console.log(data)
+  }
   return (
     <div>
       <h2>Pick add-ons</h2>
       <p>Add-ons help enhance your gaming experience.</p>
 
-      <form>
-        <AddOnInput />
-        <AddOnInput />
-        <AddOnInput />
-      </form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <AddOnInput control={control} name="customProfile" />
+        <AddOnInput control={control} name="largerStorage" />
+        <AddOnInput control={control} name="onlineService" />
 
-      <div>
-        <button type="button">Go Back</button>
-        <button type="button">Next</button>
-      </div>
+        <div>
+          <button type="button">Go Back</button>
+          <button type="submit">Next</button>
+        </div>
+      </form>
     </div>
   )
 }
--- src/components/FormInput/AddOnInput.tsx
+++ src/components/FormInput/AddOnInput.tsx
@@ -1,9 +1,29 @@
-const AddOnInput = () => {
+import { UseControllerProps, useController } from 'react-hook-form'
+import { AddOnForm } from '../Form/AddOnSection/AddOnSection.tsx'
+
+type AddOnInputProps = UseControllerProps<AddOnForm>
+
+const AddOnInput = (props: AddOnInputProps) => {
+  const { field } = useController(props)
+  console.log(field.name, field.value)
+
   return (
     <div>
-      <input type="checkbox" id="add-on" name="name of category" />
-      <label htmlFor="add-on">
-        add-on name
+      <input
+        type="checkbox"
+        id={props.name}
+        name={props.name}
+        onChange={() => {
+          if (field.value) {
+            field.onChange(false)
+          } else {
+            field.onChange(true)
+          }
+        }}
+        checked={field.value}
+      />
+      <label htmlFor={props.name}>
+        {field.name}
         <br />
         Extra text
       </label>
