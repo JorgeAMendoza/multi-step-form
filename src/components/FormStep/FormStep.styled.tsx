6416c5e67c90c21b740f0d9e0c0d59f51dac75e2
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-07-20 05:01:40 -0500

    remove test files that were no longer needed, switched module resolution to bundler, installed styled-components

---
+++ src/components/FormStep/FormStep.styled.tsx
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+
+const FormStepStyled = styled.div``
--- src/components/FormStep/FormStep.test.tsx
+++
@@ -1,7 +0,0 @@
-import { test } from 'vitest'
-import { render } from 'test/utilities.ts'
-import FormStep from './FormStep.tsx'
-
-test('component should render', () => {
-  render(<FormStep />)
-})
