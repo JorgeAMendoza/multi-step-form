1bfce04b037d97d60705204eb3a2a8571958fc1a
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-07-21 08:13:17 -0500

    create styled component for form steps, set text for mobile and desktop view, layout the list

--- src/components/FormStep/FormStep.styled.tsx
+++ src/components/FormStep/FormStep.styled.tsx
@@ -1,3 +1,28 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-const FormStepStyled = styled.div``
+export const FormStepStyled = styled.div``
+
+export const StepList = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`
+
+export const StepItem = styled.li`
+  p {
+    position: relative;
+  }
+  p:nth-of-type(1),
+  p:nth-of-type(2) {
+    display: none;
+  }
+  p:nth-of-type(3) {
+    span {
+      position: absolute;
+      top: 0;
+      left: 0;
+      visibility: hidden;
+    }
+  }
+`
--- src/components/FormStep/FormStep.tsx
+++ src/components/FormStep/FormStep.tsx
@@ -1,28 +1,40 @@
 import { useAppSelector } from '@/src/redux/hooks.tsx'
-import style from './form-step.module.css'
+import { StepItem, StepList } from './FormStep.styled'
 
 const FormStep = () => {
   const { step } = useAppSelector((state) => state.form)
   return (
     <div data-testid="formSteps">
-      <ul className={style.stepList}>
-        <li data-active={step === 'personalInfo' ? 'true' : 'false'}>
+      <StepList aria-live="polite">
+        <StepItem data-active={step === 'personalInfo' ? 'true' : 'false'}>
           <p>Step 1</p>
           <p>Your Info</p>
-        </li>
-        <li data-active={step === 'plan' ? 'true' : 'false'}>
+          <p>
+            1 <span aria-hidden="false">Your Info</span>
+          </p>
+        </StepItem>
+        <StepItem data-active={step === 'plan' ? 'true' : 'false'}>
           <p>Step 2</p>
           <p>Select Plan</p>
-        </li>
-        <li data-active={step === 'addOns' ? 'true' : 'false'}>
+          <p>
+            2 <span aria-hidden="false">Select Plan</span>
+          </p>
+        </StepItem>
+        <StepItem data-active={step === 'addOns' ? 'true' : 'false'}>
           <p>Step 3</p>
           <p>Add-Ons</p>
-        </li>
-        <li data-active={step === 'confirmation' ? 'true' : 'false'}>
+          <p>
+            3 <span aria-hidden="false">Add-Ons</span>
+          </p>
+        </StepItem>
+        <StepItem data-active={step === 'confirmation' ? 'true' : 'false'}>
           <p>Step 4</p>
           <p>Summary</p>
-        </li>
-      </ul>
+          <p>
+            4 <span aria-hidden="false">Summary</span>
+          </p>
+        </StepItem>
+      </StepList>
     </div>
   )
 }
