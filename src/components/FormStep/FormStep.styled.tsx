a2a196adbcbd7a1d9d14f8703a21ab5e3e019242
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-08-15 05:42:27 -0500

    style the form step for desktops, adjust the background image and text content

--- src/components/FormStep/FormStep.styled.tsx
+++ src/components/FormStep/FormStep.styled.tsx
@@ -1,13 +1,24 @@
 import styled from 'styled-components'
 import bgImageMobile from '@/src/assets/images/bg-sidebar-mobile.svg'
+import bgImageDesktop from '@/src/assets/images/bg-sidebar-desktop.svg'
+import device from '@/src/styles/device'
 
-console.log(bgImageMobile)
+console.log(bgImageDesktop)
 
 export const FormStepStyled = styled.div`
   min-height: 17.2rem;
   background-image: url(${bgImageMobile});
   background-repeat: no-repeat;
   background-size: cover;
+
+  @media screen and (${device.laptop}) {
+    background-image: url(${bgImageDesktop});
+    background-repeat: no-repeat;
+    background-size: cover;
+    min-height: fit-content;
+    background-position: center;
+    border-radius: 10px;
+  }
 `
 
 export const StepList = styled.ul`
@@ -16,6 +27,14 @@ export const StepList = styled.ul`
   align-items: center;
   gap: 1.6rem;
   padding: 3.2rem;
+
+  @media screen and (${device.laptop}) {
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    padding-top: 4rem;
+    gap: 3.2rem;
+  }
 `
 
 export const StepItem = styled.li`
@@ -28,17 +47,19 @@ export const StepItem = styled.li`
   align-items: center;
   color: var(--white);
   font-size: 1.4rem;
+  position: relative;
+
+  & > div {
+    display: none;
+  }
 
   p {
     position: relative;
     font-weight: bold;
     line-height: 1;
   }
-  p:nth-of-type(1),
-  p:nth-of-type(2) {
-    display: none;
-  }
-  p:nth-of-type(3) {
+
+  & > p {
     span {
       position: absolute;
       top: 0;
@@ -51,5 +72,34 @@ export const StepItem = styled.li`
     background-color: var(--sky-blue);
     color: var(--denim);
     border: 1px solid var(--sky-blue);
+  }
+
+  @media screen and (${device.laptop}) {
+    & > div {
+      display: flex;
+      flex-direction: column;
+      position: absolute;
+      width: 11rem;
+      left: 4.8rem;
+      gap: .6rem;
+    }
+
+    & > div p:first-of-type {
+      font-size: 1.2rem;
+      color: var(--light-blue);
+      font-weight: 400;
+      text-transform: uppercase;
+    }
+
+    & > div p:last-of-type {
+      font-size: 1.4rem;
+      color: var(--white);
+      text-transform: uppercase;
+      letter-spacing: 1px;
+    }
+
+    & > p span {
+      display: none;
+    }
   }
 `
--- src/components/FormStep/FormStep.tsx
+++ src/components/FormStep/FormStep.tsx
@@ -7,29 +7,38 @@ const FormStep = () => {
     <FormStepStyled data-testid="formSteps">
       <StepList aria-live="polite">
         <StepItem data-active={step === 'personalInfo' ? 'true' : 'false'}>
-          <p>Step 1</p>
-          <p>Your Info</p>
+          <div>
+            <p>Step 1</p>
+            <p>Your Info</p>
+          </div>
           <p>
             1 <span aria-hidden="false">Your Info</span>
           </p>
         </StepItem>
         <StepItem data-active={step === 'plan' ? 'true' : 'false'}>
-          <p>Step 2</p>
-          <p>Select Plan</p>
+          <div>
+            <p>Step 2</p>
+            <p>Select Plan</p>
+          </div>
+
           <p>
             2 <span aria-hidden="false">Select Plan</span>
           </p>
         </StepItem>
         <StepItem data-active={step === 'addOns' ? 'true' : 'false'}>
-          <p>Step 3</p>
-          <p>Add-Ons</p>
+          <div>
+            <p>Step 3</p>
+            <p>Add-Ons</p>
+          </div>
           <p>
             3 <span aria-hidden="false">Add-Ons</span>
           </p>
         </StepItem>
         <StepItem data-active={step === 'confirmation' ? 'true' : 'false'}>
-          <p>Step 4</p>
-          <p>Summary</p>
+          <div>
+            <p>Step 4</p>
+            <p>Summary</p>
+          </div>
           <p>
             4 <span aria-hidden="false">Summary</span>
           </p>
